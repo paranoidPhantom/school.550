5511e8be285a0f6e636d991783971b8230769e10
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-01 16:27:34 -0500

    feat: move to nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["nuxt-aos", "@nuxt/ui", "@nuxtjs/seo", "@nuxt/fonts"]
-})
+    devtools: { enabled: true },
+    modules: ["nuxt-aos", "@nuxt/ui", "@nuxtjs/seo", "@nuxt/fonts"],
+    future: {
+        compatibilityVersion: 4,
+    },
+});
