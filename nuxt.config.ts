902d7b5b1d829920430eb2d030051b03099c35de
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-01 18:28:53 -0500

    chore: move to supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["nuxt-aos", "@nuxt/ui", "@nuxtjs/seo", "@nuxt/fonts"],
+    modules: ["nuxt-aos", "@nuxt/ui", "@nuxtjs/seo", "@nuxt/fonts", "@nuxtjs/supabase"],
     future: {
         compatibilityVersion: 4,
     },
