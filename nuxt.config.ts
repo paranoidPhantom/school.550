bf13852822051c87cdcd17b15338e2848138fbae
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-02 18:44:36 -0500

    chore: add SEO info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,12 @@ export default defineNuxtConfig({
         "@nuxt/fonts",
         "@paranoidphantom/tgauth",
     ],
+    seo: {
+        url: "https://portal.ort.spb.ru",
+        name: "Школа №550",
+        description: "Оффициальный портал ГБОУ СОШ №550",
+        defaultLocale: "ru",
+    },
     future: {
         compatibilityVersion: 4,
     },
