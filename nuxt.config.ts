f80b5e9a89e0f12fe76e1c08c6bfee8027192dd6
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-01 19:05:49 -0500

    chore: remove supabase in favour of pure telegram auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["nuxt-aos", "@nuxt/ui", "@nuxtjs/seo", "@nuxt/fonts", "@nuxtjs/supabase"],
+    modules: [
+        "nuxt-aos",
+        "@nuxt/ui",
+        "@nuxtjs/seo",
+        "@nuxt/fonts",
+        "@paranoidphantom/tgauth",
+    ],
     future: {
         compatibilityVersion: 4,
+    },
+    supabase: {
+        redirectOptions: {
+            login: "/manage/auth",
+            callback: "/manage/confirm",
+            include: ["/manage*"],
+            cookieRedirect: true,
+        },
     },
 });
