62da5529ffd10afab937ed81d53889b84caf0bac
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-08 09:37:16 -0500

    feat: add caching and algolia indexing to dynamic routes

--- server/api/content/[...slug].get.ts
+++ server/api/content/[...slug].get.ts
@@ -1,14 +1,21 @@
 import { kv } from "@vercel/kv";
 
-export default defineEventHandler(async (event) => {
-    try {
-        const { slug } = getRouterParams(event);
-        const environment = process.env.VERCEL_ENV ?? process.env.NODE_ENV;
-        const md = await kv.get<string>(`${environment}_content_${slug}`);
+export default defineCachedEventHandler(
+    async (event) => {
+        try {
+            const { slug } = getRouterParams(event);
+            const environment = process.env.VERCEL_ENV ?? process.env.NODE_ENV;
+            const md = await kv.get<string>(`${environment}_content_${slug}`);
 
-        return md;
-    } catch (error) {
-        console.error("Error content/[....get:", error);
-        throw createError(error as Error);
+            return md;
+        } catch (error) {
+            console.error("Error content/[....get:", error);
+            throw createError(error as Error);
+        }
+    },
+    {
+        name: "content",
+        maxAge: 28800,
+        getKey: (event) => getRouterParams(event).slug,
     }
-});
+);
--- server/api/content/[...slug].put.ts
+++ server/api/content/[...slug].put.ts
@@ -1,4 +1,5 @@
 import { kv } from "@vercel/kv";
+import algoliasearch from "algoliasearch";
 
 export default defineEventHandler(async (event) => {
     // EDIT_CONTENT ONLY
@@ -11,8 +12,30 @@ export default defineEventHandler(async (event) => {
     try {
         const { slug } = getRouterParams(event);
         const environment = process.env.VERCEL_ENV ?? process.env.NODE_ENV;
-        const { md } = (await readBody(event)) as { md: string };
+        const { md, title, description } = (await readBody(event)) as {
+            title: string;
+            description: string;
+            md: string;
+        };
         await kv.set(`${environment}_content_${slug}`, md);
+        await useStorage("cache").removeItem(
+            `nitro:handlers:content:${slug}.json`
+        );
+
+        // @ts-expect-error
+        const algolia = algoliasearch(
+            process.env.ALGOLIA_APPLICATION_ID,
+            process.env.ALGOLIA_API_KEY
+        );
+        const index = algolia.initIndex("dev_550-portal");
+
+        index.saveObject({
+            objectID: slug,
+            title,
+            description,
+            content: md,
+            slug,
+        });
     } catch (error) {
         console.error("Error content/[...slug].put:", error);
         throw createError(error as Error);
