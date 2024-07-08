ba3a904a4374fdff3486b3fecf48299152414576
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-08 09:40:28 -0500

    chore: start work on home page design

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,27 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div>index</div>
+    <div class="__home">
+        <div class="hero">
+            <img
+                src="/images/gradient.png"
+                alt="Фон"
+                class="absolute top-0 left-0 h-screen object-cover blur-md"
+            />
+            <img
+                src="/images/noise.png"
+                class="absolute top-0 left-0 h-screen object-cover opacity-30"
+            />
+        </div>
+        test
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.__home {
+    .hero {
+        @apply w-full h-screen;
+        height: calc(100vh - 80px + 3rem);
+    }
+}
+</style>
