8e708aacb0ae1cfae638068c3ceb75dc5eddac55
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-08 09:38:51 -0500

    chore: wrap root options in single node

--- app/components/manage/option/Root.vue
+++ app/components/manage/option/Root.vue
@@ -155,188 +155,190 @@ watch(userRolePicked, (picked) => {
 </script>
 
 <template>
-    <UAlert
-        title="Корневой пользователь"
-        :color="useKeyToColor('root')"
-        variant="soft"
-        icon="ph:identification-badge-duotone"
-    />
-    <div class="flex gap-2">
-        <UPopover v-if="roles">
-            <UButton color="gray" label="Роли" />
-            <template #panel>
-                <UCard>
-                    <div class="flex flex-col gap-2 divide-y divide-gray-800">
-                        <div
-                            class="role flex gap-2 w-80 items-center pt-2"
-                            v-for="role in roles"
-                            :key="role.key"
-                        >
-                            <UBadge
-                                variant="soft"
-                                :color="useKeyToColor(role.key)"
-                                >{{ role.key }}</UBadge
-                            >
-                            <p
-                                class="overflow-hidden text-xs text-wrap text-right opacity-80"
-                                :title="role.description"
-                            >
-                                {{ role.description }}
-                            </p>
-                        </div>
-                        <div class="border-none">
-                            <UButtonGroup>
-                                <UInput
-                                    v-model="newRoleState.key"
-                                    placeholder="Роль"
-                                    size="xs"
-                                />
-                                <UInput
-                                    v-model="newRoleState.description"
-                                    placeholder="Описание"
-                                    size="xs"
-                                />
-                                <UButton
-                                    icon="mdi:plus"
-                                    size="xs"
-                                    @click="
-                                        createRole(
-                                            newRoleState.key,
-                                            newRoleState.description
-                                        )
-                                    "
-                                    color="gray"
-                                />
-                            </UButtonGroup>
-                        </div>
-                    </div>
-                </UCard>
-            </template>
-        </UPopover>
-        <UPopover v-if="users" v-model:open="popoverActive">
-            <UButton color="gray" label="Пользователи" />
-            <template #panel>
-                <UCard>
-                    <div class="flex flex-col gap-2" v-if="users">
-                        <UButtonGroup>
-                            <UInputMenu
-                                v-model="userActionState.uid"
-                                :options="userOptions"
-                                :search-attributes="['username', 'id']"
-                                value-attribute="id"
-                            />
-                            <UButton
-                                icon="line-md:edit-twotone-full"
-                                @click="
-                                    userActionState.edit = true;
-                                    popoverActive = false;
-                                "
-                                :disabled="!userFromId"
-                                color="gray"
-                            />
-                        </UButtonGroup>
-                        <UButton
-                            color="gray"
-                            icon="line-md:person-add-filled"
-                            @click="
-                                userActionState.add = true;
-                                popoverActive = false;
-                                userActionState.uid = '';
-                            "
-                            >Добавить пользователя</UButton
-                        >
-                    </div>
-                </UCard>
-            </template>
-        </UPopover>
-        <!-- Add user modal -->
-        <UModal v-model="userActionState.add">
-            <UCard>
-                <template #header>
-                    <h3 class="text-lg font-bold">Добавить пользователя</h3>
-                </template>
-				<UAlert class="mb-4" variant="subtle" color="yellow" title="Пользователь появиться только если он ранее входил" icon="mdi:alert" />
-                <div class="flex gap-2 items-center mb-4" v-if="userFromId">
-                    <UAvatar :src="userFromId.photo_url" size="lg" />
-                    <div class="flex flex-col">
-                        <h3 class="text-lg font-bold">
-                            {{ userFromId.first_name }}
-                            {{ userFromId.last_name }}
-                        </h3>
-                        <p class="text-sm text-gray-500">
-                            Создан
-                            {{
-                                useFormattedInterval(
-                                    now / 1000 - userFromId.auth_date
-                                )
-                            }}
-                            назад
-                        </p>
-                    </div>
-                </div>
-                <UButtonGroup class="w-full">
-                    <UInput
-                        class="w-full"
-                        v-model="userActionState.uid"
-                        placeholder="ID пользователя"
-                    />
-                    <UButton
-                        color="gray"
-                        label="Добавить"
-                        :disabled="!userFromId"
-                        @click="createUser"
-                    />
-                </UButtonGroup>
-            </UCard>
-        </UModal>
-        <!-- Edit user modal -->
-        <UModal v-model="userActionState.edit">
-            <UCard>
-                <template #header>
-                    <div class="flex gap-2">
-                        <UAvatar :src="userFromId.photo_url" size="lg" />
-                        <div class="info">
-                            <h3 class="text-lg font-bold">
-                                {{ userFromId.first_name }}
-                                {{ userFromId.last_name }}
-                            </h3>
-                            <p class="text-sm text-gray-500">
-                                @{{ userFromId.username }} | Создан
-                                {{
-                                    useFormattedInterval(
-                                        now / 1000 - userFromId.auth_date
-                                    )
-                                }}
-                                назад
-                            </p>
-                        </div>
-                    </div>
-                </template>
-                <h3 class="text-lg font-semibold mb-2">Роли</h3>
-                <div class="flex gap-2 items-center flex-wrap">
-                    <UBadge
-                        v-for="role in userRoles"
-                        :key="role"
-                        variant="soft"
-                        :color="useKeyToColor(role)"
-                        >{{ role }} <UButton v-if="role !== 'root'" class="ml-2" color="white" size="xs" variant="link" :padded="false" icon="mdi:close" @click="revokeUserRole(role)" /></UBadge
-                    />
-                    <UButtonGroup size="xs">
-                        <UInputMenu
-                            :options="
-                                roles.map((role: Role) => ({
-                                    label: role.key,
-                                }))
-                            "
-                            value-attribute="label"
-                            v-model="userRolePicked"
-                            placeholder="Добавить роль"
-                        />
-                    </UButtonGroup>
-                </div>
-            </UCard>
-        </UModal>
-    </div>
+	<div class="flex flex-col gap-4">
+		<UAlert
+			title="Корневой пользователь"
+			:color="useKeyToColor('root')"
+			variant="soft"
+			icon="ph:identification-badge-duotone"
+		/>
+		<div class="flex gap-2">
+			<UPopover v-if="roles">
+				<UButton color="gray" label="Роли" />
+				<template #panel>
+					<UCard>
+						<div class="flex flex-col gap-2 divide-y divide-gray-800">
+							<div
+								class="role flex gap-2 w-80 items-center pt-2"
+								v-for="role in roles"
+								:key="role.key"
+							>
+								<UBadge
+									variant="soft"
+									:color="useKeyToColor(role.key)"
+									>{{ role.key }}</UBadge
+								>
+								<p
+									class="overflow-hidden text-xs text-wrap text-right opacity-80"
+									:title="role.description"
+								>
+									{{ role.description }}
+								</p>
+							</div>
+							<div class="border-none">
+								<UButtonGroup>
+									<UInput
+										v-model="newRoleState.key"
+										placeholder="Роль"
+										size="xs"
+									/>
+									<UInput
+										v-model="newRoleState.description"
+										placeholder="Описание"
+										size="xs"
+									/>
+									<UButton
+										icon="mdi:plus"
+										size="xs"
+										@click="
+											createRole(
+												newRoleState.key,
+												newRoleState.description
+											)
+										"
+										color="gray"
+									/>
+								</UButtonGroup>
+							</div>
+						</div>
+					</UCard>
+				</template>
+			</UPopover>
+			<UPopover v-if="users" v-model:open="popoverActive">
+				<UButton color="gray" label="Пользователи" />
+				<template #panel>
+					<UCard>
+						<div class="flex flex-col gap-2" v-if="users">
+							<UButtonGroup>
+								<UInputMenu
+									v-model="userActionState.uid"
+									:options="userOptions"
+									:search-attributes="['username', 'id']"
+									value-attribute="id"
+								/>
+								<UButton
+									icon="line-md:edit-twotone-full"
+									@click="
+										userActionState.edit = true;
+										popoverActive = false;
+									"
+									:disabled="!userFromId"
+									color="gray"
+								/>
+							</UButtonGroup>
+							<UButton
+								color="gray"
+								icon="line-md:person-add-filled"
+								@click="
+									userActionState.add = true;
+									popoverActive = false;
+									userActionState.uid = '';
+								"
+								>Добавить пользователя</UButton
+							>
+						</div>
+					</UCard>
+				</template>
+			</UPopover>
+			<!-- Add user modal -->
+			<UModal v-model="userActionState.add">
+				<UCard>
+					<template #header>
+						<h3 class="text-lg font-bold">Добавить пользователя</h3>
+					</template>
+					<UAlert class="mb-4" variant="subtle" color="yellow" title="Пользователь появиться только если он ранее входил" icon="mdi:alert" />
+					<div class="flex gap-2 items-center mb-4" v-if="userFromId">
+						<UAvatar :src="userFromId.photo_url" size="lg" />
+						<div class="flex flex-col">
+							<h3 class="text-lg font-bold">
+								{{ userFromId.first_name }}
+								{{ userFromId.last_name }}
+							</h3>
+							<p class="text-sm text-gray-500">
+								Создан
+								{{
+									useFormattedInterval(
+										now / 1000 - userFromId.auth_date
+									)
+								}}
+								назад
+							</p>
+						</div>
+					</div>
+					<UButtonGroup class="w-full">
+						<UInput
+							class="w-full"
+							v-model="userActionState.uid"
+							placeholder="ID пользователя"
+						/>
+						<UButton
+							color="gray"
+							label="Добавить"
+							:disabled="!userFromId"
+							@click="createUser"
+						/>
+					</UButtonGroup>
+				</UCard>
+			</UModal>
+			<!-- Edit user modal -->
+			<UModal v-model="userActionState.edit">
+				<UCard>
+					<template #header>
+						<div class="flex gap-2">
+							<UAvatar :src="userFromId.photo_url" size="lg" />
+							<div class="info">
+								<h3 class="text-lg font-bold">
+									{{ userFromId.first_name }}
+									{{ userFromId.last_name }}
+								</h3>
+								<p class="text-sm text-gray-500">
+									@{{ userFromId.username }} | Создан
+									{{
+										useFormattedInterval(
+											now / 1000 - userFromId.auth_date
+										)
+									}}
+									назад
+								</p>
+							</div>
+						</div>
+					</template>
+					<h3 class="text-lg font-semibold mb-2">Роли</h3>
+					<div class="flex gap-2 items-center flex-wrap">
+						<UBadge
+							v-for="role in userRoles"
+							:key="role"
+							variant="soft"
+							:color="useKeyToColor(role)"
+							>{{ role }} <UButton v-if="role !== 'root'" class="ml-2" color="white" size="xs" variant="link" :padded="false" icon="mdi:close" @click="revokeUserRole(role)" /></UBadge
+						/>
+						<UButtonGroup size="xs">
+							<UInputMenu
+								:options="
+									roles.map((role: Role) => ({
+										label: role.key,
+									}))
+								"
+								value-attribute="label"
+								v-model="userRolePicked"
+								placeholder="Добавить роль"
+							/>
+						</UButtonGroup>
+					</div>
+				</UCard>
+			</UModal>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
