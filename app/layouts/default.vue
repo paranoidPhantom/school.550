a9634cf80c79cd3f0c5565f481c094d1e1700877
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-09 18:00:14 -0500

    chore: temporary footer space

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,7 +7,7 @@ defineShortcuts({
 </script>
 
 <template>
-    <div class="__layout-default">
+    <div class="__layout-default pb-32">
         <ClientHeader />
         <main class="px-4 mt-4 md:px-8">
             <slot />
