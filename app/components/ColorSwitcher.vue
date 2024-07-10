7213a6b21d7bd5b98f3c905b5184360c19b65e5c
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-09 21:30:03 -0500

    chore: wrap colorSwitcher in div

--- app/components/ColorSwitcher.vue
+++ app/components/ColorSwitcher.vue
@@ -2,42 +2,44 @@
 const colorMode = useColorMode();
 
 const modes = [
-	{
-		key: "dark",
-		icon: "line-md:sunny-outline-to-moon-alt-loop-transition",
-	},
-	{
-		key: "light",
-		icon: "line-md:moon-alt-to-sunny-outline-loop-transition",
-	},
+    {
+        key: "dark",
+        icon: "line-md:sunny-outline-to-moon-alt-loop-transition",
+    },
+    {
+        key: "light",
+        icon: "line-md:moon-alt-to-sunny-outline-loop-transition",
+    },
 ];
 
 const currentMode = computed(() => {
-	return modes.find((mode) => mode.key === colorMode.preference) || modes[0];
+    return modes.find((mode) => mode.key === colorMode.preference) || modes[0];
 });
 
 const toggleMode = () => {
-	let done = false;
-	modes.forEach((mode, index) => {
-		if (currentMode.value.key === mode.key && !done) {
-			done = true;
-			colorMode.preference = modes[(index + 1) % modes.length].key;
-		}
-	});
+    let done = false;
+    modes.forEach((mode, index) => {
+        if (currentMode.value.key === mode.key && !done) {
+            done = true;
+            colorMode.preference = modes[(index + 1) % modes.length].key;
+        }
+    });
 };
 </script>
 
 <template>
-	<ClientOnly>
-		<UButton
-			:key="currentMode.key"
-			:icon="currentMode.icon"
-			size="lg"
-			color="white"
-			variant="link"
-			@click="toggleMode"
-		/>
-	</ClientOnly>
+    <div class="color-switcher">
+        <ClientOnly>
+            <UButton
+                :key="currentMode.key"
+                :icon="currentMode.icon"
+                size="lg"
+                color="white"
+                variant="link"
+                @click="toggleMode"
+            />
+        </ClientOnly>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
