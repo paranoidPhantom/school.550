a5347dc3c2f1a0867c850d22da299ab803a5402b
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-11 12:43:41 -0500

    feat: unfiltered database operations on dev

---
+++ server/api/devonly/kv.put.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+    if (import.meta.dev) {
+        const storage = useStorage(event.context.storage_driver);
+        const { env, key, value } = await readBody(event);
+        await storage.setItem(`${env}_${key}`, value);
+    }
+});
