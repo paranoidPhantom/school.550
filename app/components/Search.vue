c8f844bbab0384203df4516464676b2cbf1a1168
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-16 11:46:27 -0500

    refactor: minimal changes in search palette

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -9,19 +9,20 @@ const enabled = useState("search_palette", () => false);
 const results = [
     {
         key: "results",
-        label: (q) => q && `Результаты "${q}"`,
+        label: (q) => q && `Результаты поиска "${q}"`,
         search: async (q) => {
             if (!q) {
                 return [];
             }
 
             const { hits } = await search({ query: q });
-
-            return hits.map((hit: Record<string, any>) => ({
-                label: hit.title,
-                suffix: hit.description,
-                to: `/${hit.slug}`,
-            }));
+            return hits.map((hit: Record<string, any>) => {
+                return {
+                    label: hit.title,
+                    suffix: hit.description,
+                    to: `/${hit.slug}`,
+                };
+            });
         },
     },
 ];
