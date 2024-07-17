7d054bdc4b048c13a5808c73db364359e80882b4
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-16 20:12:21 -0500

    feat: token factory API route

---
+++ server/api/auth/fstoken.get.ts
@@ -0,0 +1,24 @@
+import { SignJWT } from "jose";
+
+export default defineEventHandler(async (event) => {
+    const { perms } = event.context;
+    if (perms.includes("fs")) {
+        const { user } = event.context;
+        if (user && user.hasOwnProperty("id")) {
+            const secret = new TextEncoder().encode(
+                process.env.FILE_SERVER_SIGNATURE
+            );
+            const alg = "HS256";
+
+            const jwt = await new SignJWT()
+                .setProtectedHeader({ alg })
+                .setIssuedAt()
+                .setIssuer("nitro.api")
+                .setAudience(user.id)
+                .setExpirationTime("1h")
+                .sign(secret);
+
+            return jwt;
+        }
+    }
+});
