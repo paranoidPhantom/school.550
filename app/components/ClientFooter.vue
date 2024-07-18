7c5d3f094310742c5b8a2acdfc2c605b629841dc
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-17 21:43:06 -0500

    chore: add empty footer for future implementation

---
+++ app/components/ClientFooter.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <footer data-aos="fade-up" class="h-24 md:h-40">
+		
+	</footer>
+</template>
+
+<style lang="scss" scoped></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,9 +9,10 @@ defineShortcuts({
 <template>
     <div class="__layout-default pb-32">
         <ClientHeader />
-        <main class="px-4 mt-4 md:px-8">
+        <main class="px-4 mt-4 md:px-8 pb-4">
             <slot />
         </main>
+        <ClientFooter />
         <Feedback />
     </div>
 </template>
