6595036194913ef99b9fb922f02a8aa660db070f
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-18 22:06:02 -0500

    feat: implement major part of file system features
    - File uploads
    - Folder navigation
    - File viewing
    - Drag-n-drop uploads

--- app/components/manage/option/FS.vue
+++ app/components/manage/option/FS.vue
@@ -1,10 +1,18 @@
 <script lang="ts" setup>
+import type { File } from "~/types/file";
+
+const {
+    public: { file_server_url },
+} = useRuntimeConfig();
+
 const sectionActive = useCookie("admin_section_fs");
 
 const explorerState = reactive<{
     pathFragments: string[];
+    selectedFiles: Set<string>;
 }>({
     pathFragments: [],
+    selectedFiles: new Set(),
 });
 
 const currentPath = computed(() => `/${explorerState.pathFragments.join("/")}`);
@@ -12,7 +20,7 @@ const currentPath = computed(() => `/${explorerState.pathFragments.join("/")}`);
 const { data: currentPathFiles, refresh: refreshCurrentPathFiles } =
     await useAsyncData(
         `${currentPath}_list`,
-        () => useFS(`/list${currentPath.value}`, {}),
+        () => useFS(`/list${currentPath.value}`, {}) as Promise<File[]>,
         {
             server: false,
             watch: [currentPath],
@@ -22,11 +30,11 @@ const { data: currentPathFiles, refresh: refreshCurrentPathFiles } =
 const operationState = reactive<{
     active: boolean;
     opeartion: string | null;
-    files: File[];
+    files?: FileList;
 }>({
     active: false,
     opeartion: null,
-    files: [],
+    files: undefined,
 });
 
 const uploadFiles = async (files: FileList) => {
@@ -34,10 +42,24 @@ const uploadFiles = async (files: FileList) => {
     for (const file of files) {
         formData.append("files", file, file.name);
     }
-    const response = await useFS(`/`, {
-        method: "POST",
-        body: formData,
-    });
+    try {
+        operationState.active = true;
+        operationState.opeartion = "Загрузка файлов...";
+        operationState.files = files;
+        await useFS(`${currentPath.value}`, {
+            method: "POST",
+            body: formData,
+        });
+        operationState.active = false;
+    } catch (error) {
+        operationState.active = false;
+    }
+    refreshCurrentPathFiles();
+};
+
+const handleFileAction = (file: File) => {
+    if (file.isDirectory) explorerState.pathFragments.push(file.name);
+    else window.open(`${file_server_url}${currentPath.value}/${file.name}`);
 };
 </script>
 
@@ -51,26 +73,82 @@ const uploadFiles = async (files: FileList) => {
             :class="sectionActive ? 'cursor-zoom-out' : 'cursor-zoom-in'"
             @click="sectionActive = sectionActive ? '' : 'true'"
         />
-        <template v-if="sectionActive">
-            <div class="path">
-                <div class="node" @click="explorerState.pathFragments = []">
-                    <UIcon name="codicon:root-folder" />
+        <UModal v-model="operationState.active" prevent-close>
+            <UCard>
+                <template #header>
+                    <h3 class="text-xl font-semibold">
+                        {{ operationState.opeartion }}
+                    </h3>
+                </template>
+                <div
+                    v-for="file in operationState.files"
+                    class="flex items-center justify-between"
+                >
+                    <div class="flex items-center gap-2">
+                        <UIcon name="svg-spinners:ring-resize" />
+                        <p class="text-truncate">{{ file.name }}</p>
+                    </div>
+                    <p>{{ useFormattedFileSize(file.size) }}</p>
                 </div>
-                <template v-for="(node, index) in explorerState.pathFragments">
-                    <UIcon name="codicon:arrow-right" />
-                    <div
-                        class="node"
-                        @click="
-                            explorerState.pathFragments =
-                                explorerState.pathFragments.splice(0, index + 1)
-                        "
-                    >
-                        <UIcon name="codicon:folder" />
-                        <p>{{ node }}</p>
+            </UCard>
+        </UModal>
+        <template v-if="sectionActive">
+            <UCard>
+                <template #header>
+                    <div class="flex gap-4 items-center">
+                        <UButton
+                            color="gray"
+                            @click="explorerState.pathFragments = []"
+                        >
+                            <UIcon name="codicon:root-folder" />
+                        </UButton>
+                        <template
+                            v-for="(node, index) in explorerState.pathFragments"
+                        >
+                            <UIcon name="codicon:arrow-right" />
+                            <UButton
+                                class="flex items-center gap-2"
+                                color="gray"
+                                @click="
+                                    explorerState.pathFragments =
+                                        explorerState.pathFragments.splice(
+                                            0,
+                                            index + 1
+                                        )
+                                "
+                            >
+                                <UIcon name="codicon:folder" />
+                                <p>{{ node }}</p>
+                            </UButton>
+                        </template>
                     </div>
                 </template>
-            </div>
-            <UInput type="file" multiple @change="uploadFiles" />
+                <FsFileList
+                    :files="currentPathFiles"
+                    selectable
+                    @file-action="handleFileAction"
+                    @file-upload="uploadFiles"
+                    v-model:selected="explorerState.selectedFiles"
+                />
+                <template #footer>
+                    <div class="flex items-center gap-4">
+                        <UTooltip text="Загрузить файлы в выбранную папку">
+                            <UInput
+                                type="file"
+                                size="xs"
+                                icon="line-md:cloud-upload-outline-loop"
+                                class="w-fit"
+                                multiple
+                                @change="uploadFiles"
+                            />
+                        </UTooltip>
+                        <p v-show="explorerState.selectedFiles.size > 0">
+                            Выбран(о)
+                            {{ explorerState.selectedFiles.size }} файл(а)
+                        </p>
+                    </div>
+                </template>
+            </UCard>
         </template>
     </div>
 </template>
