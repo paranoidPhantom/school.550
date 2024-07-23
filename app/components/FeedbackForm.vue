19d055240c36d64a843b8fd095c36c4def0ed29d
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-07-23 17:16:17 -0500

    refactor: add loading spinner to feedback form

--- app/components/FeedbackForm.vue
+++ app/components/FeedbackForm.vue
@@ -16,17 +16,27 @@ onMounted(() => {
         false
     );
 });
+
+const spinner = ref(true);
+const hideSpinner = () => (spinner.value = false);
 </script>
 
 <template>
     <UModal v-model="feedbackForm">
         <UCard
             :ui="{
-                base: 'overflow-hidden bg-white',
-                body: { base: 'h-[600px]', padding: '!p-0' },
+                base: 'overflow-hidden',
+                body: { base: 'h-[600px] bg-white', padding: '!p-0' },
             }"
         >
+            <UIcon
+                v-if="spinner"
+                class="text-gray-900 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl z-10"
+                name="svg-spinners:6-dots-rotate"
+            />
+
             <iframe
+                @load="hideSpinner"
                 ref="iframe"
                 class="w-full h-full"
                 src="https://pos.gosuslugi.ru/form/?opaId=268828&fz59=false"
