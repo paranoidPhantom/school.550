06ae8fbc71affa6f6392be2c8f3947ff6900c645
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-08-01 18:11:15 -0500

    chore: create base article components

---
+++ app/components/content/ArticleCard.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+// to='' is also required for the link to work
+defineProps<{
+    title: string;
+    image: string;
+    date: string;
+    posx: number;
+    posy: number;
+    sizex: number;
+    sizey: number;
+}>();
+</script>
+
+<template>
+    <NuxtLink
+        class="article"
+        :style="{
+            gridRow: posy ? `${posy} / ${posy + sizey}` : `span ${sizey}`,
+            gridColumn: posx ? `${posx} / ${posx + sizex}` : `span ${sizex}`,
+        }"
+    >
+        <article>
+            <img :src="image" :alt="title" class="w-full h-full" />
+        </article>
+    </NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+.article {
+    > article {
+        @apply w-full h-full;
+        > img {
+            @apply object-cover;
+        }
+    }
+}
+</style>
---
+++ app/components/content/ArticleList.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+
+<template>
+    <section class="__articles">
+        <slot />
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.__articles {
+    display: grid;
+    // Divide the grid into as many 50px columns as possible
+    grid-template-columns: repeat(auto-fill, 50px);
+    grid-template-rows: repeat(auto-fill, 50px);
+
+    grid-auto-rows: 100px;
+
+    @apply gap-4 md:gap-8;
+}
+</style>
