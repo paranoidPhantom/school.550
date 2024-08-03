5f591efd9b60c76532157863262317ce50dda79d
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-08-02 19:05:18 -0500

    feat: add dedicated logo search page

---
+++ app/pages/logo.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+definePageMeta({
+    name: "Мы ищем логотип",
+});
+</script>
+
+<template>
+    <div class="__logo">
+		
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
