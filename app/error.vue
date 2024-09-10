2f40e1fa45eed87ffa4a76fe5dc30d8c75004748
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-09-09 21:29:51 -0500

    feat: massively improve design on error page

--- app/error.vue
+++ app/error.vue
@@ -38,18 +38,23 @@ const barcode = computed(() => `${error.statusCode} ${error.message}`);
 		<div class="creative-block">
 			<NuxtImg
 				:style="`translate: ${displacement.x}% ${displacement.y}%;`"
-				class="a hidden transition-all sm:block"
+				class="a hidden sm:block"
 				src="/images/question_mark.svg"
 			/>
 			<NuxtImg
 				:style="`translate: ${displacement.x}% ${displacement.y}%;`"
-				class="b transition-all"
+				class="b"
 				src="/images/question_mark.svg"
 			/>
 		</div>
 		<div class="error-wrapper">
 			<h1 class="text-center">
-				<span v-for="c in barcode" :key="c">{{ c }}</span>
+				<span
+					v-for="(c, index) in barcode"
+					:key="c"
+					:style="`animation-delay: ${index * 0.1}s`"
+					>{{ c }}</span
+				>
 			</h1>
 			<p class="text-center opacity-50">{{ comment }}</p>
 			<UButton size="lg" to="/" label="На главную" />
@@ -61,12 +66,31 @@ const barcode = computed(() => `${error.statusCode} ${error.message}`);
 .__error {
 	height: 100vh;
 	position: relative;
+	@keyframes letter {
+		0% {
+			opacity: 0.5;
+		}
+		50% {
+			opacity: 1;
+			font-size: 1.1em;
+		}
+		100% {
+			opacity: 0.5;
+		}
+	}
 	h1 {
-		font-size: 5rem;
-		font-weight: 200;
-		font-family: "Sankofa Display", sans-serif;
+		font-size: 2rem;
+		font-weight: 300;
+		font-family: "Source Code Pro", sans-serif;
+		max-width: 70%;
 		color: rgb(var(--color-primary-DEFAULT));
-		user-select: none;
+		span {
+			animation: letter 1s infinite 1s;
+		}
+	}
+	p {
+		font-family: "Source Code Pro", sans-serif;
+		font-weight: 200;
 	}
 	.error-wrapper {
 		position: absolute;
