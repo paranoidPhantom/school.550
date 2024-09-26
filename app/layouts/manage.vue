60b51fa058e83912c3e7f0edaa9e66bd2e2fd0f9
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-09-26 11:57:38 -0500

    chore: comment out admin header links

--- app/layouts/manage.vue
+++ app/layouts/manage.vue
@@ -5,10 +5,10 @@
 		<header
 			class="fixed left-1/2 top-0 z-10 m-2 flex -translate-x-1/2 items-center gap-4 rounded-3xl border border-gray-200 px-6 py-1 backdrop-blur-lg dark:border-gray-800"
 		>
-			<UButton variant="soft" to="/manage">Редактор</UButton>
+			<!-- <UButton variant="soft" to="/manage">Редактор</UButton>
 			<UButton variant="soft" to="/manage/learn"
 				>Ресурсы для редакторов</UButton
-			>
+			> -->
 			<ColorSwitcher />
 		</header>
 		<slot />
