c9156faf59e1ccc3659dff3697fa3b2575186a56
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-10-07 18:14:34 -0500

    chore: animate logo click

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup></script>
 
 <template>
-	<NuxtLink to="/" aria-label="Перейти на главную">
+	<NuxtLink to="/" aria-label="Перейти на главную" class="home-btn">
 		<NuxtImg src="/logo.png" width="60px" />
 	</NuxtLink>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-btn {
+	@apply transition-all duration-1000;
+	&:active {
+		@apply scale-95 transition-all duration-200;
+	}
+}
+</style>
