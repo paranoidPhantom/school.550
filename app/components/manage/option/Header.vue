1069ac80beaafd13c0bd8eff4f17168f5b4ef682
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-01-21 10:23:15 +0300

    feat: switch from tabs to buttons

--- app/components/manage/option/Header.vue
+++ app/components/manage/option/Header.vue
@@ -123,6 +123,12 @@ const saveGroup = async (index: number) => {
 		if (!error) refresh();
 	}
 };
+
+const index = ref<number>(0);
+
+const group = computed(() => {
+	return linkGroups.value ? linkGroups.value[index.value] : undefined;
+});
 </script>
 
 <template>
@@ -137,239 +143,222 @@ const saveGroup = async (index: number) => {
 		/>
 		<template v-if="sectionActive">
 			<UCard :ui="{ body: { base: 'space-y-2' } }">
-				<template #header>
-					<UTabs :items="tabs">
-						<template #item="{ item: { group }, index }">
-							<div v-if="logic[index]" class="space-y-2">
-								<UFormGroup label="Текст раздела">
-									<UInput v-model="logic[index].groupName" />
-								</UFormGroup>
-								<UFormGroup label="Высота раздела">
+				<template v-if="logic" #header>
+					<UButtonGroup class="mb-4">
+						<UButton
+							v-for="(tab, btn_index) in tabs"
+							:key="btn_index"
+							:label="tab.label"
+							:variant="index === btn_index ? 'solid' : 'link'"
+							color="gray"
+							@click="index = btn_index"
+						/>
+					</UButtonGroup>
+					<div v-if="logic[index]" class="space-y-2">
+						<UFormGroup label="Текст раздела">
+							<UInput v-model="logic[index].groupName" />
+						</UFormGroup>
+						<UFormGroup label="Высота раздела">
+							<UInput
+								v-model="logic[index].height"
+								type="number"
+							/>
+						</UFormGroup>
+						<UFormGroup label="Индекс раздела">
+							<UInput
+								v-model="logic[index].index"
+								type="number"
+							/>
+						</UFormGroup>
+						<div
+							v-if="!(logic[index].logic as Logic)?.to"
+							class="flex gap-4"
+						>
+							<UCard
+								v-for="(column, col_index) in Array.isArray(
+									logic[index].logic,
+								)
+									? logic[index].logic
+									: []"
+								:key="col_index"
+								:ui="{
+									base: 'w-fit',
+									body: { base: 'space-y-2' },
+								}"
+							>
+								<UFormGroup
+									label="Текст столбца"
+									hint="Можно оставить пустым"
+								>
 									<UInput
-										v-model="logic[index].height"
-										type="number"
+										v-model="
+											logic[index].logic[col_index].column
+										"
+										placeholder="Пусто"
+										size="xs"
 									/>
 								</UFormGroup>
-								<UFormGroup label="Индекс раздела">
+								<UFormGroup label="Индекс стобца">
 									<UInput
-										v-model="logic[index].index"
-										type="number"
+										v-model="
+											logic[index].logic[col_index].data
+												.index
+										"
+										placeholder="Пусто"
+										size="xs"
 									/>
 								</UFormGroup>
 								<div
-									v-if="!(logic[index].logic as Logic)?.to"
-									class="flex gap-4"
+									v-for="(link, link_index) in logic[index]
+										.logic[col_index].data.links"
+									:key="link_index"
+									class="flex items-center gap-4"
 								>
-									<UCard
-										v-for="(
-											column, col_index
-										) in Array.isArray(logic[index].logic)
-											? logic[index].logic
-											: []"
-										:key="col_index"
-										:ui="{
-											base: 'w-fit',
-											body: { base: 'space-y-2' },
-										}"
-									>
-										<UFormGroup
-											label="Текст столбца"
-											hint="Можно оставить пустым"
-										>
-											<UInput
-												v-model="
-													logic[index].logic[
-														col_index
-													].column
-												"
-												placeholder="Пусто"
-												size="xs"
-											/>
-										</UFormGroup>
-										<UFormGroup label="Индекс стобца">
-											<UInput
-												v-model="
-													logic[index].logic[
-														col_index
-													].data.index
-												"
-												placeholder="Пусто"
-												size="xs"
-											/>
-										</UFormGroup>
-										<div
-											v-for="(link, link_index) in logic[
-												index
-											].logic[col_index].data.links"
-											:key="link_index"
-											class="flex items-center gap-4"
-										>
-											<UInput
-												v-model="
-													logic[index].logic[
-														col_index
-													].data.links[link_index]
-														.label
-												"
-												size="xs"
-											/>
-											<UIcon name="mdi:arrow-right" />
-											<UInputMenu
-												v-model="
-													logic[index].logic[
-														col_index
-													].data.links[link_index].to
-												"
-												size="xs"
-												:options="pages"
-												value-attribute="label"
-											/>
-											<UButton
-												v-if="link_index > 0"
-												variant="soft"
-												icon="mdi:arrow-up"
-												size="xs"
-												@click="
-													() => {
-														const clicked = {
-															...logic[index]
-																.logic[
-																col_index
-															].data.links[
-																link_index
-															],
-														};
-														logic[index].logic[
-															col_index
-														].data.links[
-															link_index
-														] =
-															logic[index].logic[
-																col_index
-															].data.links[
-																link_index - 1
-															];
-
-														logic[index].logic[
-															col_index
-														].data.links[
-															link_index - 1
-														] = clicked;
-													}
-												"
-											/>
-											<UButton
-												v-if="
-													link_index <
-													logic[index].logic[
-														col_index
-													].data.links.length -
-														1
-												"
-												variant="soft"
-												icon="mdi:arrow-down"
-												size="xs"
-												@click="
-													() => {
-														const clicked = {
-															...logic[index]
-																.logic[
-																col_index
-															].data.links[
-																link_index
-															],
-														};
-														logic[index].logic[
-															col_index
-														].data.links[
-															link_index
-														] =
-															logic[index].logic[
-																col_index
-															].data.links[
-																link_index + 1
-															];
-
-														logic[index].logic[
-															col_index
-														].data.links[
-															link_index + 1
-														] = clicked;
-													}
-												"
-											/>
-											<UButton
-												icon="mdi:close"
-												color="red"
-												variant="soft"
-												size="xs"
-												@click="
-													logic[index].logic[
-														col_index
-													].data.links.splice(
-														link_index,
-														1,
-													)
-												"
-											/>
-										</div>
-										<UButtonGroup>
-											<UButton
-												label="Добавить ссылку"
-												@click="
-													logic[index].logic[
-														col_index
-													].data.links.push({
-														to: '/...',
-														label: 'Новая ссылка',
-													})
-												"
-											/>
-											<UButton
-												label="Удалить столбец (двойным кликом)"
-												color="red"
-												variant="outline"
-												@dblclick="
-													logic[index].logic.splice(
-														col_index,
-														1,
-													)
-												"
-											/>
-										</UButtonGroup>
-									</UCard>
+									<UInput
+										v-model="
+											logic[index].logic[col_index].data
+												.links[link_index].label
+										"
+										size="xs"
+									/>
+									<UIcon name="mdi:arrow-right" />
+									<UInputMenu
+										v-model="
+											logic[index].logic[col_index].data
+												.links[link_index].to
+										"
+										size="xs"
+										:options="pages"
+										value-attribute="label"
+									/>
 									<UButton
-										icon="mdi:plus"
+										v-if="link_index > 0"
 										variant="soft"
+										icon="mdi:arrow-up"
+										size="xs"
 										@click="
-											logic[index].logic.push({
-												column: 'Новый столбец',
-												data: {
-													index: 0,
-													links: [],
-												},
-											})
+											() => {
+												const clicked = {
+													...logic[index].logic[
+														col_index
+													].data.links[link_index],
+												};
+												logic[index].logic[
+													col_index
+												].data.links[link_index] =
+													logic[index].logic[
+														col_index
+													].data.links[
+														link_index - 1
+													];
+
+												logic[index].logic[
+													col_index
+												].data.links[link_index - 1] =
+													clicked;
+											}
+										"
+									/>
+									<UButton
+										v-if="
+											link_index <
+											logic[index].logic[col_index].data
+												.links.length -
+												1
+										"
+										variant="soft"
+										icon="mdi:arrow-down"
+										size="xs"
+										@click="
+											() => {
+												const clicked = {
+													...logic[index].logic[
+														col_index
+													].data.links[link_index],
+												};
+												logic[index].logic[
+													col_index
+												].data.links[link_index] =
+													logic[index].logic[
+														col_index
+													].data.links[
+														link_index + 1
+													];
+
+												logic[index].logic[
+													col_index
+												].data.links[link_index + 1] =
+													clicked;
+											}
+										"
+									/>
+									<UButton
+										icon="mdi:close"
+										color="red"
+										variant="soft"
+										size="xs"
+										@click="
+											logic[index].logic[
+												col_index
+											].data.links.splice(link_index, 1)
 										"
 									/>
 								</div>
-								<UFormGroup v-else label="Ссылка">
-									<UInput v-model="logic[index].logic.to" />
-								</UFormGroup>
 								<UButtonGroup>
 									<UButton
-										label="Сохранить"
-										@click="saveGroup(index)"
+										label="Добавить ссылку"
+										@click="
+											logic[index].logic[
+												col_index
+											].data.links.push({
+												to: '/...',
+												label: 'Новая ссылка',
+											})
+										"
 									/>
 									<UButton
+										label="Удалить столбец (двойным кликом)"
 										color="red"
 										variant="outline"
-										label="Удалить раздел (двойным кликом)"
-										@dblclick="removeGroup(group.id)"
+										@dblclick="
+											logic[index].logic.splice(
+												col_index,
+												1,
+											)
+										"
 									/>
 								</UButtonGroup>
-							</div>
-						</template>
-					</UTabs>
+							</UCard>
+							<UButton
+								icon="mdi:plus"
+								variant="soft"
+								@click="
+									logic[index].logic.push({
+										column: 'Новый столбец',
+										data: {
+											index: 0,
+											links: [],
+										},
+									})
+								"
+							/>
+						</div>
+						<UFormGroup v-else label="Ссылка">
+							<UInput v-model="logic[index].logic.to" />
+						</UFormGroup>
+						<UButtonGroup>
+							<UButton
+								label="Сохранить"
+								@click="saveGroup(index)"
+							/>
+							<UButton
+								color="red"
+								variant="outline"
+								label="Удалить раздел (двойным кликом)"
+								@dblclick="removeGroup(group.id)"
+							/>
+						</UButtonGroup>
+					</div>
 				</template>
 				<UButtonGroup>
 					<UButton
