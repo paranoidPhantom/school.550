165b9ff1c66c7fa80ef2c8a5562d412d4b80c328
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-01-31 14:51:58 +0300

    feat: add carousel

---
+++ app/components/content/ImageCarousel.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+	images: string[];
+}>();
+</script>
+
+<template>
+	<UCarousel
+		v-slot="{ item }"
+		:items="images"
+		:ui="{ item: 'basis-full' }"
+		class="overflow-hidden rounded-lg"
+	>
+		<img :src="item" draggable="false" />
+	</UCarousel>
+</template>
+
+<style lang="scss" scoped></style>
