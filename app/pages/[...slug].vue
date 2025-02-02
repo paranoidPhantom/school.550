8b8621f1a765724a9dcb0719d9813b0e5b955c0b
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-02-02 15:17:56 +0300

    fix: uncomment mostly

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,67 +1,70 @@
 <script setup lang="ts">
-// definePageMeta({
-// 	middleware: ["content"],
-// });
+definePageMeta({
+	middleware: ["content"],
+});
 
-// const {
-// 	params: { slug },
-// 	meta: { ast },
-// } = useRoute() as any;
+const {
+	params: { slug },
+	meta: { ast },
+} = useRoute() as any;
 
-// const refreshSeo = () => {
-// 	if (ast) {
-// 		useSeoMeta({
-// 			title: ast.data.title,
-// 			description:
-// 				ast.data.description === "" ? undefined : ast.data.description,
-// 		});
-// 	}
-// };
+const refreshSeo = () => {
+	if (ast) {
+		useSeoMeta({
+			title: ast.data.title,
+			description:
+				ast.data.description === "" ? undefined : ast.data.description,
+		});
+	}
+};
 
-// onMounted(refreshSeo);
-// watch(ast, refreshSeo);
+onMounted(refreshSeo);
+watch(ast, refreshSeo);
 
-// const brklinks = computed(() => {
-// 	const links = [
-// 		{ label: "Домашняя", icon: "heroicons:home-20-solid", to: "/" },
-// 	];
-// 	if (slug) {
-// 		const category = slug[0];
-// 		switch (category) {
-// 			case "info":
-// 				links.push({
-// 					label: "Сведения об ОУ",
-// 					icon: "heroicons:information-circle-20-solid",
-// 				} as { label: string; icon: string; to: string });
-// 				break;
-// 			case "for-parents":
-// 				links.push({
-// 					label: "Родителям",
-// 				} as { label: string; icon: string; to: string });
-// 				break;
-// 			case "news":
-// 				links.push({
-// 					label: "Новости",
-// 					icon: "fluent-emoji-high-contrast:rolled-up-newspaper",
-// 				} as { label: string; icon: string; to: string });
-// 				if (slug.length === 0) return links;
-// 				break;
-// 		}
-// 	}
-// 	if (slug.length > 1) {
-// 		links.push({
-// 			label: ast.data.title,
-// 		} as { label: string; icon: string; to: string });
-// 	}
+const brklinks = computed(() => {
+	const links = [
+		{ label: "Домашняя", icon: "heroicons:home-20-solid", to: "/" },
+	];
+	if (slug) {
+		const category = slug[0];
+		switch (category) {
+			case "info":
+				links.push({
+					label: "Сведения об ОУ",
+					icon: "heroicons:information-circle-20-solid",
+				} as { label: string; icon: string; to: string });
+				break;
+			case "for-parents":
+				links.push({
+					label: "Родителям",
+				} as { label: string; icon: string; to: string });
+				break;
+			case "news":
+				links.push({
+					label: "Новости",
+					icon: "fluent-emoji-high-contrast:rolled-up-newspaper",
+				} as { label: string; icon: string; to: string });
+				if (slug.length === 0) return links;
+				break;
+		}
+	}
+	if (slug.length > 1) {
+		links.push({
+			label: ast.data.title,
+		} as { label: string; icon: string; to: string });
+	}
 
-// 	return links;
-// });
+	return links;
+});
 </script>
 
 <template>
 	<div v-if="ast" :class="`__dynamic_${slug}`" class="mx-auto max-w-[1200px]">
-		<!-- <UBreadcrumb class="mb-4" :links="brklinks" />
+		<UBreadcrumb class="mb-4" :links="brklinks" />
 		<MarkdownFormatter>
+			<MDC value="# Testin the title!" />
+		</MarkdownFormatter>
+		<!-- <MarkdownFormatter>
 			<MDCRenderer :body="ast.body" :data="ast.data" />
 		</MarkdownFormatter> -->
 	</div>
