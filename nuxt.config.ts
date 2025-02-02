029688e680f8b8d2a2ba627f49eb14d2c1b32b24
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-02-02 17:21:49 +0300

    random swr changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,12 +47,12 @@ export default defineNuxtConfig({
 	],
 
 	routeRules: {
-		// "/": { swr: 60 },
-		// "/news/**": { swr: 60 },
-		// "/info/**": { swr: 60 },
-		// "/for-parents/**": { swr: 60 },
-		"/manage": { robots: false, isr: false },
-		"/manage/**": { robots: false, isr: false },
+		"/": { swr: 60 },
+		"/news/**": { swr: 60 },
+		"/info/**": { swr: 60 },
+		"/for-parents/**": { swr: 60 },
+		"/manage": { robots: false },
+		"/manage/**": { robots: false },
 	},
 
 	sitemap: {
