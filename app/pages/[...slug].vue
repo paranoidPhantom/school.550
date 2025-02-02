4f1c0c69c7622ec41d438bfdb74dfe30e2263c67
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-02-02 14:37:38 +0300

    fix: comment stuff out

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -8,54 +8,54 @@ const {
 	meta: { ast },
 } = useRoute() as any;
 
-const refreshSeo = () => {
-	if (ast) {
-		useSeoMeta({
-			title: ast.data.title,
-			description:
-				ast.data.description === "" ? undefined : ast.data.description,
-		});
-	}
-};
+// const refreshSeo = () => {
+// 	if (ast) {
+// 		useSeoMeta({
+// 			title: ast.data.title,
+// 			description:
+// 				ast.data.description === "" ? undefined : ast.data.description,
+// 		});
+// 	}
+// };
 
-onMounted(refreshSeo);
-watch(ast, refreshSeo);
+// onMounted(refreshSeo);
+// watch(ast, refreshSeo);
 
-const brklinks = computed(() => {
-	const links = [
-		{ label: "Домашняя", icon: "heroicons:home-20-solid", to: "/" },
-	];
-	if (slug) {
-		const category = slug[0];
-		switch (category) {
-			case "info":
-				links.push({
-					label: "Сведения об ОУ",
-					icon: "heroicons:information-circle-20-solid",
-				} as { label: string; icon: string; to: string });
-				break;
-			case "for-parents":
-				links.push({
-					label: "Родителям",
-				} as { label: string; icon: string; to: string });
-				break;
-			case "news":
-				links.push({
-					label: "Новости",
-					icon: "fluent-emoji-high-contrast:rolled-up-newspaper",
-				} as { label: string; icon: string; to: string });
-				if (slug.length === 0) return links;
-				break;
-		}
-	}
-	if (slug.length > 1) {
-		links.push({
-			label: ast.data.title,
-		} as { label: string; icon: string; to: string });
-	}
+// const brklinks = computed(() => {
+// 	const links = [
+// 		{ label: "Домашняя", icon: "heroicons:home-20-solid", to: "/" },
+// 	];
+// 	if (slug) {
+// 		const category = slug[0];
+// 		switch (category) {
+// 			case "info":
+// 				links.push({
+// 					label: "Сведения об ОУ",
+// 					icon: "heroicons:information-circle-20-solid",
+// 				} as { label: string; icon: string; to: string });
+// 				break;
+// 			case "for-parents":
+// 				links.push({
+// 					label: "Родителям",
+// 				} as { label: string; icon: string; to: string });
+// 				break;
+// 			case "news":
+// 				links.push({
+// 					label: "Новости",
+// 					icon: "fluent-emoji-high-contrast:rolled-up-newspaper",
+// 				} as { label: string; icon: string; to: string });
+// 				if (slug.length === 0) return links;
+// 				break;
+// 		}
+// 	}
+// 	if (slug.length > 1) {
+// 		links.push({
+// 			label: ast.data.title,
+// 		} as { label: string; icon: string; to: string });
+// 	}
 
-	return links;
-});
+// 	return links;
+// });
 </script>
 
 <template>
