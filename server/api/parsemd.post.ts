240ac4bb119e90a4c3da26571bfbf80553ea6ddf
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-02-06 16:51:38 +0300

    chore: remove unused api endpoint

--- server/api/parsemd.post.ts
+++
@@ -1,6 +0,0 @@
-import { parseMarkdown } from "#imports";
-
-export default defineEventHandler(async (event) => {
-	const { markdown } = await readBody(event);
-	return await parseMarkdown(markdown);
-});
