497f971fbf0683c358a947f48cac7e71493bdcaa
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-05-16 09:22:26 +0300

    fix: disable explosion during hydration with selective lazy hydration enabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	experimental: {
-		componentIslands: {
-			selectiveClient: true,
-		},
-	},
-
 	nitro: {
 		prerender: {
 			concurrency: 1,
