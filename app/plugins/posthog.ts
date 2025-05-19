7731f4bff50bc6acf3a0d0f27a0ed7888433cf80
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-05-20 01:45:18 +0300

    fix: posthog (check for module)

--- app/plugins/posthog.ts
+++ app/plugins/posthog.ts
@@ -2,28 +2,35 @@ import { defineNuxtPlugin } from "#app";
 import posthog from "posthog-js";
 export default defineNuxtPlugin((nuxtApp) => {
 	const runtimeConfig = useRuntimeConfig();
-	const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
-		api_host: runtimeConfig.public.posthogHost,
-		person_profiles: "always", // or 'always' to create profiles for anonymous users as well
-		capture_pageview: false, // we add manual pageview capturing below
-		loaded: (posthog) => {
-			if (import.meta.env.MODE === "development") posthog.debug();
-		},
-	});
+	if (posthog) {
+		const posthogClient = posthog.init(
+			runtimeConfig.public.posthogPublicKey,
+			{
+				api_host: import.meta.dev
+					? "http://127.0.0.1/ingest"
+					: runtimeConfig.public.posthogHost,
+				person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+				capture_pageview: false, // we add manual pageview capturing below
+				loaded: (posthog) => {
+					if (import.meta.env.MODE === "development") posthog.debug();
+				},
+			},
+		);
 
-	// Make sure that pageviews are captured with each route change
-	const router = useRouter();
-	router.afterEach((to) => {
-		nextTick(() => {
-			posthog.capture("$pageview", {
-				current_url: to.fullPath,
+		// Make sure that pageviews are captured with each route change
+		const router = useRouter();
+		router.afterEach((to) => {
+			nextTick(() => {
+				posthog.capture("$pageview", {
+					current_url: to.fullPath,
+				});
 			});
 		});
-	});
 
-	return {
-		provide: {
-			posthog: () => posthogClient,
-		},
-	};
+		return {
+			provide: {
+				posthog: () => posthogClient,
+			},
+		};
+	}
 });
