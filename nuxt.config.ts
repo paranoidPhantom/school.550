7033495de7e79e6b7632dd6df0ae3ebb452e59da
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-05-27 16:52:40 +0300

    feat: hide preview domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,10 @@ export default defineNuxtConfig({
 		"nuxt-posthog",
 	],
 
+  robots: {
+    disallow: process.env.ROBOTS_NO_INDEX ? ["/**"] : []
+  },
+
 	routeRules: {
 		"/manage": { robots: false },
 		"/manage/**": { robots: false },
