d5a8a1173ea5cc9e49550c2e37bfe02d6fcb4b98
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-07-15 15:08:02 -0500

    fix: dev server failing on start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,9 +40,9 @@ export default defineNuxtConfig({
 		"nuxt-posthog",
 	],
 
-  robots: {
-    disallow: process.env.ROBOTS_NO_INDEX ? ["/**"] : []
-  },
+	robots: {
+		disallow: process.env.ROBOTS_NO_INDEX ? ["/**"] : [],
+	},
 
 	routeRules: {
 		"/manage": { robots: false },
@@ -101,6 +101,12 @@ export default defineNuxtConfig({
 		},
 		cookieOptions: {
 			secure: false,
+		},
+	},
+
+	vite: {
+		optimizeDeps: {
+			include: ["debug"],
 		},
 	},
 
